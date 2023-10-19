2110cf3be5055786c03bacb3fce36092a5acf469
Tyakuma-Game <yyang4945@gmail.com>
2023-10-19 21:12:12 +0900

    プレイヤーの定数クラスを制作しました

---
+++ Assets/Scripts/Player/Status/PlayerConstants.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ƒvƒŒƒCƒ„[‚Ì’è”‚ğŠÇ—‚·‚éƒNƒ‰ƒX
+/// </summary>
+public class PlayerConstants : MonoBehaviour
+{
+    //|||||||||||||||||||||/
+    // ‹“_ŠÖ˜A
+    //|||||||||||||||||||||/
+    [Header("‹“_ŠÖ˜A")]
+    [Tooltip("Player‚Ì‹“_ã‰º‰ñ“]”ÍˆÍ")]
+    [SerializeField] float VERTICAL_ROTATION_RANGE = 60f;
+
+    /// <summary>
+    /// Player‚Ì‹“_ã‰º‰ñ“]”ÍˆÍ
+    /// </summary>
+    public float VerticalRotationRange
+    {
+        get { return VERTICAL_ROTATION_RANGE; }
+    }
+
+    [Tooltip("‹“_ˆÚ“®‚Ì‘¬“x")]
+    [SerializeField] float MOUSE_SENSITIVITY = 1f;
+
+    /// <summary>
+    /// ‹“_ˆÚ“®‚Ì‘¬“x
+    /// </summary>
+    public float MouseSensitivity
+    {
+        get { return MOUSE_SENSITIVITY; }
+    }
+
+    //|||||||||||||||||||||/
+    // ‘Ì—ÍŠÖ˜A
+    //|||||||||||||||||||||/
+    [Header("‘Ì—ÍŠÖ˜A")]
+    [Tooltip("Player‚ÌHPÅ‘å’l")]
+    [SerializeField] int PLAYER_MAX_HP = 100;
+
+    /// <summary>
+    /// Player‚ÌHPÅ‘å’l
+    /// </summary>
+    public int MaxHP
+    {
+        get { return PLAYER_MAX_HP; }
+    }
+
+    //|||||||||||||||||||||/
+    // ˆÚ“®ŠÖ˜A
+    //|||||||||||||||||||||/
+    [Header("ˆÚ“®ŠÖ˜A")]
+    [Tooltip("Player‚Ì•à‚«‘¬“x")]
+    [SerializeField] float PLAYER_WALK_SPEED = 4f;
+
+    /// <summary>
+    /// Player‚Ì•à‚«‘¬“x
+    /// </summary>
+    public float WalkSpeed
+    {
+        get { return PLAYER_WALK_SPEED; }
+    }
+
+    [Tooltip("Player‚Ì‘–‚è‘¬“x")]
+    [SerializeField] float PLAYER_RUN_SPEED = 8f;
+
+    /// <summary>
+    /// Player‚Ì‘–‚è‘¬“x
+    /// </summary>
+    public float RunSpeed
+    {
+        get { return PLAYER_RUN_SPEED; }
+    }
+
+    [Tooltip("Player‚ÌƒWƒƒƒ“ƒv—Í")]
+    [SerializeField] Vector3 PLAYER_JUMP_FORTH = new Vector3(0, 3f, 0);
+
+    /// <summary>
+    /// Player‚ÌƒWƒƒƒ“ƒv—Í
+    /// </summary>
+    public Vector3 JumpForce
+    {
+        get { return PLAYER_JUMP_FORTH; }
+    }
+
+    [Tooltip("Player‚Ì‰ñ“]‘¬“x")]
+    [SerializeField] float ROTATION_SPEED = 5.0f;
+
+    /// <summary>
+    /// Player‚Ì‰ñ“]‘¬“x
+    /// </summary>
+    public float RotationSpeed
+    {
+        get { return ROTATION_SPEED; }
+    }
+}
