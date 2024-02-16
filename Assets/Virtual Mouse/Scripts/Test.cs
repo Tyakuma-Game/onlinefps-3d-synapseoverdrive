9c0a8176eafb34d9b8528491c0ff4e458b0e8a18
Tyakuma-Game <yyang4945@gmail.com>
2024-02-16 12:00:13 +0900

    仮想マウスの実装が終わりました。

---
+++ Assets/Virtual Mouse/Scripts/Test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+   public void OnButtonTestLog()
+    {
+        Debug.Log("ƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚Ü‚µ‚½I");
+    }
+}
